3f6420830d8ee84a185d1c6b16394d3c66e40b8e
naseffipaid <senafirdissa58@gmail.com>
2025-09-26 12:57:10 -0700

    Hyper parameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -40,7 +40,41 @@ class ModelTrainer:
                 "Lasso": Lasso(),
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
-            model_report:dict = evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            ## hyper parameter tuning
+            params = {
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression": {},
+                "K-Neighbors Regressor": {
+                    'n_neighbors':[3,5,7,9,11],
+                    'weights':['uniform','distance'],
+                    'algorithm':['auto','ball_tree','kd_tree']
+                },
+                "Ridge": {
+                    'alpha':[0.1,0.01,0.001,0.0001],
+                    'solver':['svd','cholesky','lsqr','sparse_cg','sag']
+                },
+                "Lasso": {
+                    'alpha':[0.1,0.01,0.001,0.0001],
+                    'selection':['cyclic','random']
+                },  
+                "AdaBoost Regressor": {
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            }
+
+
+            model_report:dict = evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,params=params)
 
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
